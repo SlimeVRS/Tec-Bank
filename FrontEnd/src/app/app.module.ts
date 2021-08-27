97cca157da1d6befb912c0c8826ad71f6a5768df
Dr.Garcia <48070831+traznar@users.noreply.github.com>
2021-08-26 19:34:57 -0600

    Bootstrap linkeado
    Probablemente lo tenga que hacer cada uno

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RolesComponent } from './roles/roles.component';
+import { ClientsComponent } from './clients/clients.component';
+import { AccountsComponent } from './accounts/accounts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RolesComponent,
+    ClientsComponent,
+    AccountsComponent
   ],
   imports: [
     BrowserModule,
