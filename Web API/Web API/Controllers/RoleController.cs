7f4417635198eb748a4dd5f8579dff1fe89312f8
Marco Rivera <marco99eu@gmail.com>
2021-09-03 16:53:07 -0600

    API Added
    Just get method

---
+++ Web API/Web API/Controllers/RoleController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Newtonsoft.Json;
+using Web_API.Models;
+
+namespace Web_API.Controllers
+{
+    public class RoleController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            Console.WriteLine("This is a get response");
+            var response = JsonConvert.DeserializeObject(File.ReadAllText("F:\\Progras\\Web API\\Web API\\DataBase\\Roles.json"));
+            return Request.CreateResponse(HttpStatusCode.OK, response);
+        }
+
+        public string Post(Role role)
+        {
+            try
+            {
+                var data = JsonConvert.DeserializeObject(File.ReadAllText("F:\\Progras\\Web API\\Web API\\DataBase\\Roles.json"));
+                Console.WriteLine(role);
+                return "Added succesfully!!!";
+            }
+            catch (Exception)
+            {
+                return "Something went wrong...";
+            }
+        }
+    }
+}
---
+++ Web API/Web API/Models/Cards.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web_API.Models
+{
+    public class Cards
+    {
+        public String cardNumber { get; set; }
+        public String cardType { get; set; }
+        public String cardExpiryDate { get; set; }
+        public String cardSecureCode { get; set; }
+        public String cardBalance { get; set; }
+    }
+}
